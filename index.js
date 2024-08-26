b7ec6d7187d2bcc710196c4acad79a49e7d3e3c1
mohammed-alshaibani <20160129@su.edu.ye>
2024-08-26 14:44:53 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Add your Circle class here
+class Circle {
+    constructor(radius) {
+      this.radius = radius;
+    }
+  
+    get diameter() {
+      return this.radius * 2;
+    }
+  
+    set diameter(diameter) {
+      this.radius = diameter / 2;
+    }
+  
+    get circumference() {
+      return Math.PI * this.diameter;
+    }
+  
+    set circumference(circumference) {
+      this.radius = circumference / (Math.PI * 2);
+    }
+  
+    get area() {
+      return Math.PI * Math.pow(this.radius, 2);
+    }
+  
+    set area(area) {
+      this.radius = Math.sqrt(area / Math.PI);
+    }
+  }
+  
+  // Example Usage:
+  const circle = new Circle(5);
+  
+  console.log(circle.diameter);       
+  console.log(circle.circumference);  
+  console.log(circle.area);           
+  
+  circle.diameter = 14;
+  console.log(circle.radius);         
+  
+  circle.circumference = 62.83185307179586;
+  console.log(circle.radius);         
+  
+  circle.area = 314.1592653589793;
+  console.log(circle.radius);        
+  
